440daf0e39682b68b0ef4c8a9019d4aec7979cdc
WaseeKhan <mdvaseem014@gmail.com>
2026-02-25 13:45:49 +0530

    Added Spring Boot Devtools for auto restart

--- src/main/java/com/hisaablite/controller/HomeController.java
+++ src/main/java/com/hisaablite/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.hisaablite.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "dashboard";
-    }
+public String home(Model model, Authentication authentication) {
+    model.addAttribute("username", authentication.getName());
+    return "dashboard";
+}
 }
